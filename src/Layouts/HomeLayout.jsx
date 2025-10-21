7accab5f0e2658062d78512b3b8204752b030214
khandakershahi <khandakershahi@gmail.com>
2025-10-21 23:32:00 +0600

    Boost Application Performance & Hide Credentials

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
 import LeftAside from '../components/homelayout/LeftAside';
 import RightAside from '../components/homelayout/RightAside';
+import Loading from '../Pages/Loading';
 
 const HomeLayout = () => {
+    const { state } = useNavigation();
     return (
         <div>
             <header>
@@ -28,9 +30,9 @@ const HomeLayout = () => {
                     </LeftAside>
                 </aside>
                 <section className='main col-span-6'>
-                    <Outlet>
+                    {state == 'loading' ? <Loading></Loading> : <Outlet>
 
-                    </Outlet>
+                    </Outlet>}
                 </section>
                 <aside className='col-span-3 sticky top-0 h-fit'>
                     <RightAside></RightAside>
